c6a11f7a83bb41d03af5faf87f4670ccea7082bc
MuhammadAli084 <muhammadali.ilpex@gamil.com>
2021-06-15 15:18:01 -0700

    third change

--- js/animation.js
+++ js/animation.js
@@ -3,8 +3,10 @@ $(document).ready(function() {
     gsap.to("header ul a", 1, { y: 0, duration: 18, stagger: 0.1 });
     gsap.to(".green-border", 1, { x: 0, duration: 25, });
     gsap.to(".block", 1, { y: 0, duration: 1, stagger: 0.5 });
-    gsap.to(".card", 1, { y: 0, duration: 10, stagger: 1 });
-    gsap.to(".heading,.heading::after", 1, { x: -100, duration: 10, stagger: 1 });
+    gsap.to(".card", 5, { y: 0, duration: 20, stagger: 1 });
+    gsap.to(".border", 5, { scaleX: 1, duration: 30, });
+    gsap.to("h2 span", 1, { y: 0, duration: 5, stagger: 0.2 });
+
 
 
     // let tls = gsap.timeline({
