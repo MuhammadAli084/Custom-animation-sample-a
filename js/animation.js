47d085c3fbcce69a567b8b67ffcf27fc3bca7718
MuhammadAli084 <muhammadali.ilpex@gamil.com>
2021-08-10 15:05:59 -0700

    home page animation

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,19 @@
 $(document).ready(function() {
+    gsap.from(".black-border", {
+        scrollTrigger: {
+            trigger: "header",
+            toggleActions: "restart none resume reverse",
+            // toggleActions: "play complete reverse reset"
+        },
+        scaleX: 0,
+        duration: 10,
+        repeat: -1,
+        transformOrigin: "center",
+        ease: "none"
+    });
     gsap.to(".logo span", 1, { y: 0, duration: 5, stagger: 0.2 });
     gsap.to("header ul a", 1, { y: 0, duration: 18, stagger: 0.1 });
-    gsap.to(".black-border", 3, { scaleX: 1, duration: 25, });
+    // gsap.to(".black-border", 3, { scaleX: 1, duration: 25, });
     gsap.to(".block", 1, { y: 0, duration: 1, stagger: 0.5 });
     gsap.to(".card", 5, { y: 0, duration: 20, stagger: 1 });
     gsap.to(".border", 5, { scaleX: 1, duration: 30, });
